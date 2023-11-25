bd5e50da3e148a32571f22765783c776c041f80d
Your Name <you@example.com>
2023-11-25 17:02:20 +0300

    refactor: change CORS

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -17,7 +17,7 @@ func StartServer() {
 	
 	app.Use(cors.New(cors.Config{
         AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-        AllowOrigins:     "https://todo-app-jet-seven.vercel.app/",
+        AllowOrigins:     "*",
         AllowCredentials: true,
         AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
     }))
